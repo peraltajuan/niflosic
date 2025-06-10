6610ecf1fd4c115dc6f107943fbfcd7cc34ab9de
Juan E. Peralta <43978385+peraltajuan@users.noreply.github.com>
2025-06-10 13:24:26 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setuptools.setup(
      name="niflosic",
      author = "Juan E. Peralta",
      version="0.0.1",
-     python_requires=">=3.10", 
+     python_requires=">=3.6", 
      requires = ["setuptools", "pyscf", "numpy", "rdkit", "scipy"],
      packages=["niflosic"],
      py_modules=["niflosic"]
